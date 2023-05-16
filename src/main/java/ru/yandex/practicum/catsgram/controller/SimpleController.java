f74d3fffaa049ac2238af540a91feaf0957996cf
Galina <Galya_iv@mail.ru>
2023-05-16 11:10:51 +0300

    Обработка ситуации, если БД не работает

--- src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
@@ -1,19 +1,32 @@
 package ru.yandex.practicum.catsgram.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-//import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.catsgram.service.HackCatService;
 
+import java.util.Optional;
 
 @RestController
 public class SimpleController {
-    private static final Logger log = LoggerFactory.getLogger(SimpleController.class);
+
+    private final HackCatService hackCatService;
+
+    public SimpleController(HackCatService hackCatService) {
+        this.hackCatService = hackCatService;
+    }
+
+    // внедрите бин HackCatService
+
+    @GetMapping("/do-hack")
+    public Optional<String> doHack() {
+        // хакните этих котиков
+        return hackCatService.doHackNow()
+                .map(password -> "Пароль: " + password + " - верный")
+                .or(() -> Optional.of("Не удалось подобрать правильный пароль"));
+    }
 
     @GetMapping("/home")
     public String homePage() {
-        log.debug("Получен запрос.");
         return "Котограм";
     }
 }
